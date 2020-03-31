f77dcdab29206f7a140fd69d5c0280be4570c0d6
solidhtwoo <174594@stu.hebut.edu.cn>
2020-03-31 12:37:23 +0800

    refactored querystudentscore

--- src/main/java/com/onlinejudge/examservice/ExamServiceQueryStudentScore.java
+++ src/main/java/com/onlinejudge/examservice/ExamServiceQueryStudentScore.java
@@ -1,14 +1,17 @@
 package com.onlinejudge.examservice;
 
-import com.onlinejudge.util.DatabaseUtil;
 import com.onlinejudge.util.IntegerEvent;
+import com.onlinejudge.util.InternalException;
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.*;
-import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -22,6 +25,9 @@ public class ExamServiceQueryStudentScore extends IntegerEvent {
     private String studentID;
     private String examID;
     private int queryType;
+    private Connection conn = null;
+    private PreparedStatement stmt = null;
+    private ResultSet result = null;
     private static final Logger logger = LoggerFactory.getLogger(ExamServiceQueryStudentScore.class);
 
     public ExamServiceQueryStudentScore(String examID, String studentID, int queryType) {
@@ -31,94 +37,102 @@ public class ExamServiceQueryStudentScore extends IntegerEvent {
     }
 
     private boolean judge(Timestamp time, Timestamp startTime, Timestamp endTime) {
-        boolean result;
+        boolean res;
         if (this.queryType == 1) {
-            result = time.after(endTime) || time.before(startTime);
+            res = time.after(endTime) || time.before(startTime);
         } else {
-            result = time.before(endTime) && time.after(startTime);
+            res = time.before(endTime) && time.after(startTime);
         }
-        return result;
+        return res;
     }
 
-    public int go() {
-        Connection conn = null;
-        PreparedStatement stmt = null;
-        ResultSet result = null;
-        try {
-            conn = DatabaseUtil.getConnection();
-            stmt = prepareStatement("select * from exam e where e.eid = ?");
-            stmt.setString(1, this.examID);
+    @NotNull
+    @Contract(" -> new")
+    private Pair<Timestamp, Timestamp> checkExamID() throws SQLException, InternalException {
+        Timestamp startTime = null;
+        Timestamp endTime = null;
+        stmt = prepareStatement("select * from exam e where e.eid = ?");
+        stmt.setString(1, this.examID);
+        result = stmt.executeQuery();
+        int cnt = 0;
 
-            // Query 1: get exam from examID;
+        while (result.next()) {
+            ++cnt;
+            startTime = result.getTimestamp("estart");
+            endTime = result.getTimestamp("eend");
+        }
+        if (cnt == 0) {
+            logger.warn("ExamID:{} doesn''t exist.", this.examID);
+            throw new InternalException("exam doesn't exist");
+        } else if (cnt > 1) {
+            logger.warn("There are one more exam with ExamID:{}, something wrong must happened to database", this.examID);
+            logger.warn("Please, check the database for further information");
+            throw new InternalException("database corrupted");
+        }
+        result.close();
+        stmt.close();
+        return new ImmutablePair<>(startTime, endTime);
+    }
+
+    @NotNull
+    private List<String> getProblemID() throws SQLException {
+        List<String> probIDs = new ArrayList<>();
+        // Query 2: querying the exam and examprob, search for the probID(s)
+        stmt = prepareStatement("select ep.pid from exam e, examprob ep where e.eid = ep.eid and e.eid = ?");
+        stmt.setString(1, this.examID);
+        int cnt = 0;
+        result = stmt.executeQuery();
+        while (result.next()) {
+            ++cnt;
+            probIDs.add(result.getString("pid"));
+        }
+        if (cnt == 0) {
+            logger.warn("Oops... Seems that there are no problem(s) in this exam, what's your problem?");
+        }
+        result.close();
+        stmt.close();
+        return probIDs;
+    }
+
+    private int getScore(@NotNull Pair<Timestamp, Timestamp> pair, @NotNull List<String> probIDs) throws SQLException {
+        // Query 3: querying the submission, search for student's last submission.
+        int ret = 0;
+        HashMap<String, Integer> score = new HashMap<>();
+        Timestamp startTime = pair.getLeft();
+        Timestamp endTime = pair.getRight();
+        int cnt = probIDs.size();
+        stmt = prepareStatement("select * from submission s where s.suid = ? and s.seid = ? and s.spid = ? ");
+        for (int i = 1; i <= cnt; ++i) {
+            //qry = String.format("select * from submission s where s.suid = '%s' and s.seid = '%s' and s.spid = '%s'", this.studentID, this.examID, probIDs.get(i - 1))
+            stmt.setString(1, this.studentID);
+            stmt.setString(2, this.examID);
+            stmt.setString(3, probIDs.get(i - 1));
+            logger.info(stmt.toString());
             result = stmt.executeQuery();
-            int cnt = 0;
-            Timestamp startTime = null, endTime = null;
+            int co = 0;
             while (result.next()) {
-                ++cnt;
-                startTime = result.getTimestamp("estart");
-                endTime = result.getTimestamp("eend");
-            }
-            if (cnt == 0) {
-                logger.warn(MessageFormat.format("ExamID:{0} doesn''t exist.", this.examID));
-                return -1;
-            } else if (cnt > 1) {
-                logger.warn(MessageFormat.format("There are one more exam with ExamID:{0}, something wrong must happened to database", this.examID));
-                logger.warn("Please, check the database for further information");
-                return -1;
-            }
-            result.close();
-            //Exam currentExam = new Exam();
-            //String qry = String.format();
-
-            // Query 2: querying the exam and examprob, search for the probID(s)
-            stmt.close();
-            stmt = prepareStatement("select ep.pid from exam e, examprob ep where e.eid = ep.eid and e.eid = ?");
-            stmt.setString(1, this.examID);
-            cnt = 0;
-            result = stmt.executeQuery();
-            List<String> probIDs = new ArrayList<>();
-            while (result.next()) {
-                ++cnt;
-                probIDs.add(result.getString("pid"));
-            }
-            if (cnt == 0) {
-                logger.warn("Oops... Seems that there are no problem(s) in this exam, what's your problem?");
-                return -1;
-            }
-            result.close();
-            stmt.close();
-
-            // Query 3: querying the submission, search for student's last submission.
-            HashMap<String, Integer> score = new HashMap<>();
-            stmt = prepareStatement("select * from submission s where s.suid = ? and s.seid = ? and s.spid = ? ");
-            for (int i = 1; i <= cnt; ++i) {
-                //qry = String.format("select * from submission s where s.suid = '%s' and s.seid = '%s' and s.spid = '%s'", this.studentID, this.examID, probIDs.get(i - 1));
-                stmt.setString(1, this.studentID);
-                stmt.setString(2, this.examID);
-                stmt.setString(3, probIDs.get(i - 1));
-                logger.debug(stmt.toString());
-                result = stmt.executeQuery();
-                int co = 0;
-                while (result.next()) {
-                    Timestamp time = result.getTimestamp("stime");
-                    if (judge(time, startTime, endTime)) {
-                        continue;
-                    }
-                    ++co;
-                    var cscore = result.getInt("sscore");
-                    String pid = result.getString("spid");
-                    score.putIfAbsent(pid, cscore);
-                    var pscore = score.get(pid);
-                    if (pscore < cscore) score.replace(pid, cscore);
+                Timestamp time = result.getTimestamp("stime");
+                if (judge(time, startTime, endTime)) {
+                    continue;
                 }
-                logger.info("find {} results", co);
-
+                ++co;
+                var cscore = result.getInt("sscore");
+                String pid = result.getString("spid");
+                score.putIfAbsent(pid, cscore);
+                var pscore = score.get(pid);
+                if (pscore < cscore) score.replace(pid, cscore);
             }
-            int ret = 0;
-            for (String key : probIDs) score.putIfAbsent(key, 0);
-            for (String key : probIDs) ret += score.get(key);
-            closeQuery(result, stmt, conn);
-            return ret;
+            logger.info("find {} results", co);
+        }
+        for (String key : probIDs) score.putIfAbsent(key, 0);
+        for (String key : probIDs) ret += score.get(key);
+        closeQuery(result, stmt);
+        return ret;
+    }
+
+    public int go() throws InternalException {
+        try {
+            return getScore(checkExamID(), getProblemID());
         } catch (SQLException se) {
             logger.error("Something wrong in SQL", se);
             try {
--- src/main/java/com/onlinejudge/userservice/UserServiceListTimeline.java
+++ src/main/java/com/onlinejudge/userservice/UserServiceListTimeline.java
@@ -1,4 +1,57 @@
 package com.onlinejudge.userservice;
 
-public class UserServiceListTimeline {
+import com.onlinejudge.util.ListEvent;
+import lombok.extern.log4j.Log4j2;
+import org.jetbrains.annotations.NotNull;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.onlinejudge.util.DatabaseUtil.closeQuery;
+import static com.onlinejudge.util.DatabaseUtil.prepareStatement;
+
+@Log4j2
+public class UserServiceListTimeline extends ListEvent<SendableTimelineItem> {
+    private static final int COMMENT_MAX_LENGTH = 19;
+    private final String userID;
+
+    public UserServiceListTimeline(@NotNull String userID) {
+        this.userID = userID;
+    }
+
+    @Override
+    public List<SendableTimelineItem> go() {
+        PreparedStatement stmt = null;
+        ResultSet ret = null;
+        List<SendableTimelineItem> result = new ArrayList<>();
+        try {
+            stmt = prepareStatement("select * from timeline where uid = ?");
+            stmt.setString(1, userID);
+            log.debug(stmt);
+            ret = stmt.executeQuery();
+            while (ret.next()) {
+                String name = ret.getString("name");
+                String description = ret.getString("description");
+                Timestamp ts = ret.getTimestamp("time");
+                int type = ret.getInt("type");
+                if (description.length() >= 20) {
+                    description = description.substring(0, COMMENT_MAX_LENGTH) + "...";
+                }
+                result.add(new SendableTimelineItem(new TimelineItem(name, description, type, userID, ts)));
+            }
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+        } finally {
+            try {
+                closeQuery(ret, stmt);
+            } catch (SQLException e) {
+                log.error(e.getMessage(), e);
+            }
+        }
+        return result;
+    }
 }
