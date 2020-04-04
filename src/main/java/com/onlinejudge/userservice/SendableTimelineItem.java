99b1af4929cddc69870f3e233fd789b0dd695ae0
solidhtwoo <174594@stu.hebut.edu.cn>
2020-04-04 10:33:24 +0800

    bug fix

--- src/main/java/com/onlinejudge/userservice/SendableTimelineItem.java
+++ src/main/java/com/onlinejudge/userservice/SendableTimelineItem.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import org.jetbrains.annotations.NotNull;
 
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Getter
 @Setter
@@ -13,7 +14,7 @@ public class SendableTimelineItem {
     private String itemDescription;
     private int itemType;
     private String uid;
-    private String itemTIme;
+    private String itemTime;
 
     public SendableTimelineItem(@NotNull TimelineItem t) {
         this.itemName = t.getName();
@@ -21,7 +22,7 @@ public class SendableTimelineItem {
         this.itemType = t.getType();
         this.uid = t.getUid();
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        itemTIme = formatter.format((t.getTime().toLocalDateTime()));
+        itemTime = formatter.format((new Date(t.getTime().getTime())));
     }
 
 }
